e84f711faa4993febf00f3d497f41c8fe9be3d9b
schiacci <ben@opinyun.com>
2012-07-27 18:47:33 -0700

    updated README for version and runtime examples added login,logout duplicate handling to bag.py added test cases to unit test for bag.py deleted a log file

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-pig --param input=u_ex120718\ 1.log,u_ex120718\ 2.log,u_ex120718\ 3.log,u_ex120718\ 4.log  log_processor.pig
+HadoopVersion: 0.20.3-dev	
+PigVersion:    0.10.0
+
+
+Example run:
+hadoop dfs -put logs/u_ex120718\ 1.log u_ex120718\ 1.log;hadoop dfs -put logs/u_ex120718\ 2.log u_ex120718\ 2.log;hadoop dfs -put logs/u_ex120718\ 3.log u_ex120718\ 3.log;hadoop dfs -put logs/u_ex120718\ 4.log u_ex120718\ 4.log;pig --param input=1,2,3,4 log_processor.pig
+
+parameters input: '1,2,3,4' is the X in u_ex120718\ X.log.  I was having problems with spaces.  Typically, I try to replace spaces with underscores during normal data processing.
+
+unit test of mapper:
+python bag_unit_test.py
--- bag.py
+++ bag.py
@@ -2,6 +2,8 @@
 def login_logout_sequence(records):
   date_time_uriStem_ip_seconds_list = []
   # Read each bag item and append a time in estimated seconds
+  seconds_dateTimeUriStemIpSeconds_map = {}
+  found_duplicate                      = False
   for record in records:
     year,month,day      = record[0].split('-')
     hour,minute,seconds = record[1].split(':')
@@ -10,7 +12,51 @@ def login_logout_sequence(records):
     date_time_uriStem_ip_seconds = [record[0], record[1], record[2], record[3], seconds]
     date_time_uriStem_ip_seconds_list.append(date_time_uriStem_ip_seconds)
 
-  # loop through each item in order
+    if seconds in seconds_dateTimeUriStemIpSeconds_map:
+      found_duplicate = True
+    if seconds not in seconds_dateTimeUriStemIpSeconds_map:
+      seconds_dateTimeUriStemIpSeconds_map[seconds] = []
+    seconds_dateTimeUriStemIpSeconds_map[seconds].append(date_time_uriStem_ip_seconds)
+
+  #=================================================================================================
+  # Handle duplicate order login first then logout
+  #=================================================================================================
+  if found_duplicate:
+    reordered_date_time_uriStem_ip_seconds_list = []
+    for seconds, date_time_uriStem_ip_seconds_list in seconds_dateTimeUriStemIpSeconds_map.iteritems():
+      if len(date_time_uriStem_ip_seconds_list) == 1:
+        reordered_date_time_uriStem_ip_seconds_list.append(date_time_uriStem_ip_seconds_list[0])
+      else:
+        temp_date_time_uriStem_ip_seconds_list = date_time_uriStem_ip_seconds_list
+        cont = True
+        while len(temp_date_time_uriStem_ip_seconds_list) > 0:
+          appended_login = False
+          for possible_login in date_time_uriStem_ip_seconds_list:
+            if possible_login[2] == '/Login' and possible_login in temp_date_time_uriStem_ip_seconds_list:
+              temp_date_time_uriStem_ip_seconds_list.remove(possible_login)
+              reordered_date_time_uriStem_ip_seconds_list.append(possible_login)
+              appended_login = True
+              break
+          if appended_login == False:
+            break
+           
+          appended_logout = False
+          for possible_logout in date_time_uriStem_ip_seconds_list:
+            if possible_logout[2] == '/Logout' and possible_logout in temp_date_time_uriStem_ip_seconds_list:
+              temp_date_time_uriStem_ip_seconds_list.remove(possible_logout)
+              reordered_date_time_uriStem_ip_seconds_list.append(possible_logout)
+              appended_logout = True
+              break
+          if appended_logout == False:
+            break
+        for date_time_uriStem_ip_seconds in temp_date_time_uriStem_ip_seconds_list:
+          reordered_date_time_uriStem_ip_seconds_list.append(date_time_uriStem_ip_seconds)
+    date_time_uriStem_ip_seconds_list = reordered_date_time_uriStem_ip_seconds_list
+  #=================================================================================================
+
+  #=================================================================================================
+  # loop through each item in order in seconds (stable sort for duplicates seconds)
+  #=================================================================================================
   previous_login       = False
   ip_login_logout_list = []
   current_ip           = None
